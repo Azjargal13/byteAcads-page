975633aaa8008b8c4f2c3b185f8676509dd83770
Azjargal13 <azjargal.gankhuyag@virtusize.com>
2021-05-18 20:21:46 +0900

    navigation fix

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,9 @@
 <template>
-  <nav class="flex items-center justify-between flex-wrap p-8">
-    <!-- <app-logo></app-logo> -->
+  <div
+    class="flex items-center justify-between flex-wrap p-8 place-item-center"
+  >
     <div>
-      <svg width="200" height="100">
+      <svg width="200" height="70">
         <rect width="200" height="70" rx="8" ry="8" style="fill:#fbe134;" />
         <text x="10" y="50" fill="#0b0c0c" class="logoText">ByteAcads</text>
       </svg>
@@ -12,51 +13,55 @@
         @click="toggleMenu"
         class="flex items-center px-3 py-2 border rounded text-grey-lightest border-grey-lightest hover:text-main-green hover:border-main-green focus:outline-none"
       >
-        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <title>Menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
       </button>
     </div>
-    <div
-      class="w-full text-gray-200 lg:flex items-center lg:w-auto sm:block"
-      v-bind:class="{'xs:hidden': menu}"
+    <nav
+      class="w-full text-gray-200 lg:flex items-center lg:w-auto "
+      v-bind:class="{ 'nav-menu-mb': menu, hidden: !menu }"
     >
       <a
         href="#"
         v-scroll-to="'#whoWeAre'"
-        class="block mt-4 lg:inline-block lg:mt-0 sm:text-right font-medium no-underline hover:text-main-green mr-4"
-      >who we are</a>
+        class="block mt-4 lg:inline-block lg:mt-0 sm:text-right font-medium no-underline hover:text-main-green hover:underline mr-4"
+        >who we are</a
+      >
       <a
         href="#"
         v-scroll-to="'#whatWeDo'"
-        class="block mt-4 lg:inline-block sm:text-right lg:mt-0 font-medium no-underline text-grey-lightest hover:text-main-green mr-4"
-      >what we do</a>
+        class="block mt-4 lg:inline-block sm:text-right lg:mt-0 font-medium no-underline text-grey-lightest hover:underline hover:text-main-green mr-4"
+        >what we do</a
+      >
       <a
         href="#"
         v-scroll-to="'#whyWe'"
-        class="block mt-4 lg:inline-block sm:text-right lg:mt-0 font-medium no-underline text-grey-lightest hover:text-main-green mr-4"
-      >why ByteAcads?</a>
-    </div>
-  </nav>
+        class="block mt-4 lg:inline-block sm:text-right lg:mt-0 font-medium no-underline text-grey-lightest hover:underline  hover:text-main-green mr-4"
+        >why ByteAcads?</a
+      >
+    </nav>
+  </div>
 </template>
 
 <script>
-// import AppLogo from "./AppLogo.vue"
 export default {
-  components: {
-    // AppLogo
-  },
+  components: {},
   data() {
     return {
-      menu: true
+      menu: false,
     };
   },
   methods: {
     toggleMenu() {
       this.menu = !this.menu;
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>
@@ -64,5 +69,8 @@ export default {
   font-size: 36px;
   font-weight: bold;
   font-family: "Quicksand", sans-serif;
+}
+.nav-menu-mb {
+  flex-direction: column;
 }
 </style>
